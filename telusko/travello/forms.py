e86c4b792a0d0e5a85bf6939fd661eb61691a8c2
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-01 12:18:05 -0500

    Haciendo formulario de la aplicación para ingresar lugares

--- telusko/travello/forms.py
+++ telusko/travello/forms.py
@@ -1,9 +1,7 @@
 from django import forms
-from .models import Alumno, Curso, NotasAlumnosPorCurso
+from .models import Destination
 
-class IngresarLugar(forms.ModelForm):
-  
-  class Meta:
-    model = Alumno
-    fields = ['cui', 'nombre', 'apellidos', 'edad', 'dni']
-    
+class DestinationForm(forms.ModelForm):
+    class Meta:
+        model = Destination
+        fields = ['nombreCiudad', 'descripcionCiudad', 'imagenCiudad', 'precioTour', 'ofertaTour']
