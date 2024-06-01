9d1fd486db184eecd4c7d702ac58904cbe5c2afa
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-01 13:44:07 -0500

    Reorganizando forms para que se encuentren en la plantilla admin

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -18,12 +18,32 @@ def news(request):
     return render(request, 'news.html');
     
 def admin(request):
+    crear_form = DestinationForm()
+    eliminar_form = DestinationForm()
+    modificar_form = DestinationForm()
+    listar_form = DestinationForm()
+    
     if request.method == 'POST':
-        form = DestinationForm(request.POST)
-        if form.is_valid():
-          form.save()
-          return redirect('admin')
-    else:
-        form = DestinationForm()
-    return render(request, 'admin.html', {'form': form})
+        if 'crear' in request.POST:
+            crear_form = DestinationForm(request.POST, request.FILES)
+            if crear_form.is_valid():
+                crear_form.save()
+                return redirect('admin')
+        elif 'eliminar' in request.POST:
+            
+            pass
+        elif 'modificar' in request.POST:
+            
+            pass
+        elif 'listar' in request.POST:
+            
+            pass
+    
+    context = {
+        'crear_form': crear_form,
+        'eliminar_form': eliminar_form,
+        'modificar_form': modificar_form,
+        'listar_form': listar_form,
+    }
+    return render(request, 'admin.html', context)
     
