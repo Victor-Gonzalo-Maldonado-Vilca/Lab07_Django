4f801f041208e7e4cd46173a379462de8f3ba380
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-01 05:16:12 -0500

    Haciendo pruebas cambiando nombres

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html', {'price': 700});
+    dest1 = Destination()
+    dest1.name = 'Perú'
+    return render(request, 'index.html', {'dest1': dest1});
